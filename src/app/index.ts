89953b1a5de6f5eb941eb17f1a90aa5ade617867
jeffreyvnzanten <j.van.zanten@gmail.com>
2025-03-20 16:25:33 +0100

    should inject

--- src/app/index.ts
+++ src/app/index.ts
@@ -29,8 +29,8 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 
-  runMigrations();
-  // insertTestData();
+  // runMigrations();
+  insertTestData();
 });
 
 async function insertTestData() {
