9d295ff8e4a668949afc31c5c48d77a60826d16c
jeffreyvnzanten <j.van.zanten@gmail.com>
2025-05-16 09:03:28 +0200

    update

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,13 +3,9 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./drizzle",
-  schema: "./src/db/schema.ts",
+  schema: "./src/db/schema/*",
   dialect: "postgresql",
   dbCredentials: {
-    host: process.env.DB_HOST || "localhost",
-    port: parseInt(process.env.DB_PORT || "5432"),
-    user: process.env.DB_USER || "remote_user",
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || "todos",
+    url: process.env.DB_URL!,
   },
 });
--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -4,10 +4,10 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 
-const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+const connectionString = process.env.DB_URL!;
 const migrationClient = postgres(connectionString, { max: 1 });
 
-export async function runMigrations() {
+async function runMigrations() {
   try {
     await migrate(drizzle(migrationClient), {
       migrationsFolder: "./drizzle",
@@ -18,3 +18,5 @@ export async function runMigrations() {
     console.error("Failed to run migrations:", error);
   }
 }
+
+runMigrations();
