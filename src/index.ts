9add6394844d8ef8e609f950ab83255b0594faa5
jeffreyvnzanten <j.van.zanten@gmail.com>
2025-05-16 09:16:11 +0200

    run scripts + db insert

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,15 @@ import { db } from "./db";
 
 const port = parseInt(process.env.PORT ?? "3000", 10);
 
-// async function todoTest() {
-//   const todos = await getAllTodos();
-//   console.log("Todos:", todos);
-// }
+async function todoTest() {
+  const todos = await getAllTodos();
+  console.log("Todos:", todos);
+}
 
-// insertTestData();
+insertTestData();
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port ${port}`);
   console.log(db);
-  // todoTest();
+  todoTest();
 });
