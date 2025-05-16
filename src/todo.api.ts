4d583f5eb90e2713fadd06285fc034a5e0139f11
jeffreyvnzanten <j.van.zanten@gmail.com>
2025-05-16 10:43:46 +0200

    addtodo endpoint #1

--- src/todo.api.ts
+++ src/todo.api.ts
@@ -11,13 +11,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", async (req: Request, res: Response) => {
+app.get("/", async (res: Response) => {
   res.json({ status: "get /: ok" });
 });
 
-app.get("/todos", async (req: Request, res: Response) => {
+app.get("/todos", async (res: Response) => {
   const todos = await getAllTodos();
   res.json(todos);
+});
+
+app.post("addtodo", async (req: Request, res: Response) => {
+  try {
+    createTodo(req.body);
+
+    res.json({ status: "ok" });
+  } catch (error) {
+    console.error("Error creating todo:", error);
+    res.status(500).json({ error: "Failed to create todo" });
+  }
 });
 
 export default app;
