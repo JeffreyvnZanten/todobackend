3ac2b8fe8b238a4e74d09deef0939024f5306a30
jeffreyvnzanten <j.van.zanten@gmail.com>
2025-05-16 10:52:35 +0200

    getalltodos werkt weer nu addtodo testen

--- src/todo.api.ts
+++ src/todo.api.ts
@@ -15,9 +15,18 @@ app.get("/", async (req: Request, res: Response) => {
   res.json({ status: "get /: ok" });
 });
 
-app.get("/todos", async (res: Response) => {
+app.get("/todos", async (req: Request, res: Response) => {
   const todos = await getAllTodos();
   res.json(todos);
+});
+
+app.post("/addtodo", async (req: Request, res: Response) => {
+  try {
+    createTodo(req.body);
+    res.status(201).json({ message: "Todo created successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to create todo", error });
+  }
 });
 
 export default app;
