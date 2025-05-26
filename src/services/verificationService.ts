800552eb54aac77a2dcf77a67dbf79ae580f75fd
jeffreyvnzanten <j.van.zanten@gmail.com>
2025-05-26 10:07:17 +0200

    filter functie tussenstap

--- src/services/verificationService.ts
+++ src/services/verificationService.ts
@@ -3,11 +3,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const WHITELIST = (process.env.WHITELIST ?? "")
-  .split(",")
-  .map((e) => e.trim().toLowerCase())
-  .filter(Boolean);
+const WHITELIST = process.env.WHITELIST || "";
 
+function filteredWhiteList() {
+  const filteredWhiteList = WHITELIST.split(",").map((e) =>
+    e.trim().toLowerCase()
+  );
+
+  return filteredWhiteList;
+}
 export function checkEmailWhitelist(
   req: Request,
   res: Response,
@@ -15,8 +19,9 @@ export function checkEmailWhitelist(
 ): void {
   console.log("whitelist triggered, toegestaan:", WHITELIST);
   const email = (req.body.email ?? "").toLowerCase();
-  if (!WHITELIST.includes(email)) {
+  if (!filteredWhiteList().includes(email)) {
     res.status(403).json({ error: "E-mail niet toegestaan op dit platform." });
     return;
   }
+  next();
 }
