1e7474e21751629b4d002dba7a0800f036038d35
jeffreyvnzanten <j.van.zanten@gmail.com>
2025-05-26 10:19:05 +0200

    and new again

--- src/services/verificationService.ts
+++ src/services/verificationService.ts
@@ -1,15 +1,25 @@
 import { Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
 
-const WHITELIST = ["test@example.com"];
+dotenv.config();
 
+const WHITELIST = process.env.WHITELIST || "";
+
+function filteredWhiteList() {
+  const filteredWhiteList = WHITELIST.split(",").map((e) =>
+    e.trim().toLowerCase()
+  );
+
+  return filteredWhiteList;
+}
 export function checkEmailWhitelist(
   req: Request,
   res: Response,
   next: NextFunction
 ): void {
-  console.log("whitelist triggered");
+  console.log("whitelist triggered, toegestaan:", WHITELIST);
   const email = (req.body.email ?? "").toLowerCase();
-  if (!WHITELIST.includes(email)) {
+  if (!filteredWhiteList().includes(email)) {
     res.status(403).json({ error: "E-mail niet toegestaan op dit platform." });
     return;
   }
