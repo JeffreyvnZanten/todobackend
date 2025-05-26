7cf1ea9f2000f7c7ec16eaefcb826e873242c91d
jeffreyvnzanten <j.van.zanten@gmail.com>
2025-05-26 10:09:00 +0200

    back to working version

--- src/services/verificationService.ts
+++ src/services/verificationService.ts
@@ -1,25 +1,15 @@
 import { Request, Response, NextFunction } from "express";
-import dotenv from "dotenv";
 
-dotenv.config();
+const WHITELIST = ["test@example.com"];
 
-const WHITELIST = process.env.WHITELIST || "";
-
-function filteredWhiteList() {
-  const filteredWhiteList = WHITELIST.split(",").map((e) =>
-    e.trim().toLowerCase()
-  );
-
-  return filteredWhiteList;
-}
 export function checkEmailWhitelist(
   req: Request,
   res: Response,
   next: NextFunction
 ): void {
-  console.log("whitelist triggered, toegestaan:", WHITELIST);
+  console.log("whitelist triggered");
   const email = (req.body.email ?? "").toLowerCase();
-  if (!filteredWhiteList().includes(email)) {
+  if (!WHITELIST.includes(email)) {
     res.status(403).json({ error: "E-mail niet toegestaan op dit platform." });
     return;
   }
